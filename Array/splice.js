09a90f9eb4f9778f70a213dd144857c18786b1f5
CodeBurner <codeburner0@gmail.com>
2023-06-25 19:45:35 +0530

    splice

---
+++ Array/splice.js
@@ -0,0 +1,7 @@
+let arr=[1,2,3,4,4,7,8,6,4,3,1,5]
+arr.splice(1,2)
+console.log(arr)
+arr.splice(1,1,3,3,3,3,3)
+console.log(arr)
+//change the array and return array with new items
+//arr.slice(strating_index,delete count,elements_for_adding)
